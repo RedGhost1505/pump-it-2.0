d8ec009b28afebf582d58ba0dd61a37cca18a343
RedGhost1505 <joshalejandro117@gmail.com>
2024-10-13 19:22:39 -0600

    New home button

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -7,21 +7,29 @@ const Footer = () => {
 
 
     return (
-        <footer className="bg-white bg-opacity-45 w-[300px] h-16 my-4 rounded-full">
+        <footer className="bg-white bg-opacity-45 w-[400px] h-16 my-4 rounded-full">
             <div className="container mx-auto h-full flex justify-center items-center relative">
                 {/* Fondo animado */}
                 <motion.div
-                    className={`absolute h-[75%] mx-5 rounded-full bg-red-600`}
+                    className="absolute h-[75%] mx-5 rounded-full bg-red-600"
                     style={{
-                        // Usamos left o right dependiendo del enlace activo
-                        left: activeLink === 'About' ? '0%' : '41.5%', // Ajusta las posiciones según tu diseño
-                        width: activeLink === 'About' ? '6rem' : '8rem', // Cambia el ancho aquí si es necesario
+                        left: activeLink === 'Home' ? '2%' : activeLink === 'About' ? '27.5%' : '57.5%',
+                        width: activeLink === 'Home' ? '5rem' : activeLink === 'About' ? '6rem' : '7.5rem',
                     }}
-                    layoutId="background" // Esto permite la animación de posición
-                    transition={{ type: 'spring', stiffness: 300, damping: 20 }} // Ajusta el tipo de transición aquí
+                    layoutId="background"
+                    transition={{ type: 'spring', stiffness: 300, damping: 20 }}
                 />
 
                 <div className="flex justify-between w-full px-10 z-10">
+                    <motion.a
+                        href="#"
+                        onClick={() => setActiveLink('Home')}
+                        className={`font-semibold text-lg ${activeLink === 'Home' ? 'text-white' : 'text-black'
+                            }`}
+                        whileTap={{ scale: 0.95 }} // Efecto de escala al hacer clic
+                    >
+                        Home
+                    </motion.a>
                     <motion.a
                         href="#"
                         onClick={() => setActiveLink('About')}
